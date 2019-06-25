9a936202be9c40d03bb3011ca7608a9c46689bb6
Noskcaj <kcajdev@gmail.com>
2019-06-24 21:42:38 -0400

    Memoize context_from_surface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use log::{debug, error, info, trace};
 
-use rlua::{Function, Lua};
+use rlua::{Function, Lua, Table};
 
 mod config;
 mod platform;
@@ -20,11 +20,21 @@ fn init_lua(polymer: &Polymer) -> rlua::Result<()> {
 
         let connect_signal = lua.create_function(signals::connect_signal)?;
         let emit_signal = lua.create_function(signals::emit_signal)?;
+        let context_from_surface: Function = lua
+            .load(
+                r#"
+                function(surface)
+                    local cairo = require('lgi').cairo
+                    return cairo.Context(cairo.Surface(surface))
+                end"#,
+            )
+            .eval()?;
 
         let polymer_table = lua.create_table()?;
 
         polymer_table.set("connect_signal", connect_signal)?;
         polymer_table.set("emit_signal", emit_signal)?;
+        polymer_table.set("context_from_surface", context_from_surface)?;
 
         lua.globals().set("__polymer_sys", polymer_table)?;
 
@@ -60,14 +70,10 @@ fn draw(polymer: &Polymer, cr: &cairo::Context, width: f64, height: f64) {
             let cairo_surface_ptr = surface.to_raw_none();
 
             let get_context_fn: Function = lua
-                .load(
-                    r#"
-                function(surface)
-                    local cairo = require('lgi').cairo
-                    return cairo.Context(cairo.Surface(surface))
-                end"#,
-                )
-                .eval()?;
+                .globals()
+                .get::<_, Table>("__polymer_sys")?
+                .get("context_from_surface")?;
+
             let context: rlua::Value =
                 get_context_fn.call((rlua::LightUserData(cairo_surface_ptr as *mut _),))?;
 
